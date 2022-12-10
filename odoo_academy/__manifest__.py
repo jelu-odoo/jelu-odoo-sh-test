272bc538d42f736a25fed45de61dcd43783c7364
Jeffrey Luo <jelu@odoo.com>
2022-12-10 00:21:05 +0000

    [ADD] Refresh

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -13,7 +13,20 @@
     'website': 'https://www.odoo.com', 
     'category': 'Training',
     'version': '0.1', 
-    'depends': ['base'], 
-    'data': [], 
-    'demo': [], 
+    'depends': ['sale', 'website'], 
+    'data': [
+        'security/academy_security.xml',
+        'security/ir.model.access.csv',
+        'views/academy_menuitems.xml',
+        'views/course_views.xml',
+        'views/session_views.xml',
+        'views/sale_views_inherit.xml',
+        'views/product_views_inherit.xml',
+        'wizard/sale_wizard_view.xml',
+        'reports/session_report_templates.xml',
+        'views/academy_web_templates.xml',
+    ], 
+    'demo': [
+        'demo/academy_demo.xml',
+    ], 
 }
