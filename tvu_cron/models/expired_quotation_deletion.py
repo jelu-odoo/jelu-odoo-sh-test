3a64ec873151adedaeae340e45707e282f648c96
Jeffrey Luo <jelu@odoo.com>
2022-12-13 18:27:26 +0000

    [FIX] class name

--- tvu_cron/models/expired_quotation_deletion.py
+++ tvu_cron/models/expired_quotation_deletion.py
@@ -3,7 +3,7 @@
 from odoo import models, fields, api
 
 class ExpiredQuotationDeletion(models.Model): 
-    _name = "tvu_cron.expired_quotation_deletion"
+    _name = "expired_quotation_deletion"
     _description = "test description"
     
     def _remove_expired_quotations(self): 
@@ -11,6 +11,7 @@ class ExpiredQuotationDeletion(models.Model):
         quotations = self.env['sale.order'].search([['state','=','draft']])
         # check if the sale orders field "is_expired"
         for q in quotations: 
+            q.validity_date 
             q.action_cancel()
             # non-empty, date value
                 
