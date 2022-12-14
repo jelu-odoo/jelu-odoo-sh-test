629f635e707012e620c34288bf1370f60f256aed
jelu <jelu@odoo.com>
2022-12-14 17:42:25 +0000

    [ADD] first commit

---
+++ borgam/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'POS-required VAT & Phone', 
+    'summary': """
+        Point of Sale required VAT & Phone
+    """, 
+    'description': """
+        Point of Sale required VAT & Phone
+    """, 
+    'author': 'Odoo',
+    'website': 'https://www.odoo.com', 
+    'category': 'Training',
+    'version': '0.1', 
+    'depends': ['point_of_sale', 'sale_management', 'sale'], 
+    'data': [
+    ], 
+    'demo': [
+    ], 
+}
