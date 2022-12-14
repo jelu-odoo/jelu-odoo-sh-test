f1aa7892cd3482dbfdd863c266b0902e1f47b8a1
jelu@odoo.com <jelu@odoo.com>
2022-12-14 21:20:16 +0000

    [ADD] first commit

---
+++ matrix_systems/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Matrix Systems : Sequential Number for Barcode', 
+    'summary': """
+        Matrix Systems : Sequential Number for Barcode
+    """, 
+    'description': """
+        Matrix Systems : Sequential Number for Barcode
+    """, 
+    'author': 'Odoo',
+    'website': 'https://www.odoo.com', 
+    'category': 'Training',
+    'version': '0.1', 
+    'depends': ['sale_management', 'sale'], 
+    'data': [
+    ], 
+    'demo': [
+    ], 
+}
