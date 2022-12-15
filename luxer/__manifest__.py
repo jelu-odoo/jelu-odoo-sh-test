6c0146dca4428d6f69a52d9a77073078a2e3573a
jelu@odoo.com <jelu@odoo.com>
2022-12-15 18:00:04 +0000

    [ADD] first commit

---
+++ luxer/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Adding res.partner to Subscription & Invoice', 
+    'summary': """
+        Adding res.partner to Subscription & Invoice
+    """, 
+    'description': """
+        Adding res.partner to Subscription & Invoice
+    """, 
+    'author': 'Odoo',
+    'website': 'https://www.odoo.com', 
+    'category': 'Training',
+    'version': '0.1', 
+    'depends': ['sale_subscription', 'sale_management', 'sale'], 
+    'data': [
+    ], 
+    'demo': [
+    ], 
+}
