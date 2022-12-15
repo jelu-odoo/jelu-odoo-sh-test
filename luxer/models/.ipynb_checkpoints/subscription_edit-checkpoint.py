1a859d2fbfd268ae5a31e5b43ce7e95aec940214
jelu@odoo.com <jelu@odoo.com>
2022-12-15 18:08:53 +0000

    [ADD] first commit

--- luxer/models/.ipynb_checkpoints/subscription_edit-checkpoint.py
+++ luxer/models/.ipynb_checkpoints/subscription_edit-checkpoint.py
@@ -1,4 +1,6 @@
 from odoo import models, fields, api
 
 class SubscriptionEdit(models.Model): 
+    _name = "luxer.subscription_edit"
+    _description = "Adding res.partner field to Subscription"
     pass 
