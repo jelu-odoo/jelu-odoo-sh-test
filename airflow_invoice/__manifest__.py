e77066cd522a4cca4e90fb279add61d6f206f43b
jelu@odoo.com <jelu@odoo.com>
2022-12-20 18:06:29 +0000

    [ADD] first commit

---
+++ airflow_invoice/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Invoice Editing', 
+    'summary': """
+Changes Invoice fields
+    """, 
+    'description': """
+        Module that manages Invoice changing
+    """, 
+    'author': 'Odoo',
+    'website': 'https://www.odoo.com', 
+    'category': 'Training',
+    'version': '0.1', 
+    'depends': ['sale_management', 'account', 'website'], 
+    'data': [
+    ], 
+    'demo': [
+    ], 
+}
