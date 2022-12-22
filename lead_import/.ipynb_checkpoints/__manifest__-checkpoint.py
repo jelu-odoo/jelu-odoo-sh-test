53866513600096ff1e4c6b3989792a452b84dfec
jelu@odoo.com <jelu@odoo.com>
2022-12-22 18:01:39 +0000

    [ADD] first commit

---
+++ lead_import/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Lead Import', 
+    'summary': """imports leads from an excel file""", 
+    'description': """
+        Script that imports leads from an excel file
+    """, 
+    'author': 'Odoo',
+    'website': 'https://www.odoo.com', 
+    'category': 'Training',
+    'version': '0.1', 
+    'depends': ['crm'], 
+    'data': [
+    ], 
+    'demo': [
+    ], 
+}
