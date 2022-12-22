7b6215b1d0e7abb4441045bd3ddf5ea3f4fc5722
jelu@odoo.com <jelu@odoo.com>
2022-12-22 17:09:05 +0000

    [ADD] First commit

---
+++ rfq_cron/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Scheduled Action for RFQ Approval', 
+    'summary': """
+Scheduled Action for RFQ Approval""", 
+    'description': """
+        Scheduled Action for RFQ Approval
+    """, 
+    'author': 'Odoo',
+    'website': 'https://www.odoo.com', 
+    'category': 'Training',
+    'version': '0.1', 
+    'depends': ['sale_management', 'purchase'], 
+    'data': [
+    ], 
+    'demo': [
+    ], 
+}
